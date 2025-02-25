69de99e9331c2988bb0937994a87b45716106c98
Robert Sullivan <rsullivan@unc.edu>
2025-02-25 17:06:52 -0500

    Switch CHAT_HEADER and PAGE_TITLE
    PAGE_TITLE should contain less characters for UI. CHAT_HEADER should begin with Chat with ... or append the ${AI_NAME}

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Satori`;
+export const CHAT_HEADER: string = `Chat with Satori`;
 export const MESSAGE_PLACEHOLDER: string = `... *meditation noises* ...`;
 export const FOOTER_MESSAGE: string = `Satori is a custom AI assistant designed to introduce you to the ancient practice of meditation. Created by Robert Sullivan.`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
-export const PAGE_TITLE: string = `Chat with Satori`;
+export const PAGE_TITLE: string = `Satori`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
