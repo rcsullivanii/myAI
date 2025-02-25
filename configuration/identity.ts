5734f51000f464ffc6749c18150abb80ee3db4a9
Robert Sullivan <rsullivan@unc.edu>
2025-02-25 17:24:44 -0500

    Update AI_TONE and AI_ROLE

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Robert Sullivan`;
 export const OWNER_DESCRIPTION: string = `${OWNER_NAME} is a senior at the Univ of North Carolina at Chapel Hill studying computer science.`;
 
 export const AI_NAME: string = `Satori`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_TONE: string = `Serene`;
+export const AI_ROLE: string = `Meditation expert designed to teach users how to achieve the benefits of the practice.`;
