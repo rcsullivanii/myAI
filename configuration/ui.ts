e137f6fb383cd760984459b0bb63010701de1ca2
Robert Sullivan <rsullivan@unc.edu>
2025-02-25 17:01:34 -0500

    Update CHAT_HEADER in ui.ts
    Shorten CHAT_HEADER for better UI on window with many tabs

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Chat with Satori`;
+export const CHAT_HEADER: string = `Satori`;
 export const MESSAGE_PLACEHOLDER: string = `... *meditation noises* ...`;
 export const FOOTER_MESSAGE: string = `Satori is a custom AI assistant designed to introduce you to the ancient practice of meditation. Created by Robert Sullivan.`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
