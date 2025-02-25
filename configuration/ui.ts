c7b48303dd02a6d2f87e1db00942377c0cc71b44
Robert Sullivan <rsullivan@unc.edu>
2025-02-25 16:58:50 -0500

    Replace placeholders in ui.ts
    Update all placeholders in ui.ts to customized for Satori chatbot.

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const CHAT_HEADER: string = `Chat with Satori`;
+export const MESSAGE_PLACEHOLDER: string = `... *meditation noises* ...`;
+export const FOOTER_MESSAGE: string = `Satori is a custom AI assistant designed to introduce you to the ancient practice of meditation. Created by Robert Sullivan.`;
+export const CLEAR_BUTTON_TEXT: string = `Clear`;
+export const PAGE_TITLE: string = `Chat with Satori`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
