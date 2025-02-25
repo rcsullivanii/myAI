085a3e4af02d1fba22bae15c58b2439e7d8b7cb3
Robert Sullivan <rsullivan@unc.edu>
2025-02-25 17:13:04 -0500

    Update OWNER_DESCRIPTION

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Robert Sullivan`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `${OWNER_NAME} is a senior at the Univ of North Carolina at Chapel Hill studying computer science.`;
 
 export const AI_NAME: string = `Satori`;
 export const AI_TONE: string = `[AI TONE]`;
